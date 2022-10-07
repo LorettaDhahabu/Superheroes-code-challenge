9a84b22fdee79ee8081165593e686fb52e3f0eae
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-07 09:22:19 +0300

    get a power and return an error if the power is not found

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,7 +1,21 @@
 class PowersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :power_not_found
     # GET /powers
     def index
         powers = Power.all
         render json: powers
+    end
+
+    #  GET /powers/:id
+    def show
+        power = Power.find(params[:id])
+        render json: power
+    end
+
+
+    private
+
+    def power_not_found
+        render json: {error: "Power not found"}, status: :not_found
     end
 end
