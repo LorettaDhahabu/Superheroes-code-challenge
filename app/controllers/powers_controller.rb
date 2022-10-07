e2592ba567621436023013f4855e8c7f0f261a44
loretta dhahabu <loreydjefwa@gmail.com>
2022-10-07 10:11:37 +0300

    update power description if it passes the validation

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,21 +1,40 @@
 class PowersController < ApplicationController
-rescue_from ActiveRecord::RecordNotFound, with: :power_not_found
-    # GET /powers
-    def index
-        powers = Power.all
-        render json: powers
-    end
+  rescue_from ActiveRecord::RecordNotFound, with: :power_not_found
 
-    #  GET /powers/:id
-    def show
-        power = Power.find(params[:id])
-        render json: power
-    end
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
 
+  # GET /powers
+  def index
+    powers = Power.all
+    render json: powers
+  end
 
-    private
+  #  GET /powers/:id
+  def show
+    power = Power.find(params[:id])
+    render json: power
+  end
 
-    def power_not_found
-        render json: {error: "Power not found"}, status: :not_found
-    end
+  # PATCH /powers/:id
+  def update
+    #f ind
+    power = Power.find(params[:id])
+    # update
+    power.update!(power_params)
+    render json: power
+  end
+
+  private
+
+  def power_params
+    params.permit(:id, :name, :description)
+  end
+
+  def unprocessable_entity(invalid)
+    render json: { errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def power_not_found
+    render json: { error: "Power not found" }, status: :not_found
+  end
 end
